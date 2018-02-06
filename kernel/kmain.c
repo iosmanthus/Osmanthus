c73fae83eb62b22cd1a8b5e176816cee8f523863
iosmanthus <myosmanthustree@gmail.com>
2018-02-06 18:11:34 +0800

    Add some tests

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -9,11 +9,5 @@ u32 write( u8, u8, u8, u32 );
 int kmain( struct multiboot *mboot_ptr )
 {
   kconsole_clear();
-  for ( int i = 0; i < 25; ++i )
-    for ( int j = 0; j < 80; ++j )
-      kconsole_puts( (const u8 *)"c" );
-
-  for ( int j = 0; j < 81; ++j )
-    kconsole_puts( (const u8 *)"O" );
   return 0;
 }
