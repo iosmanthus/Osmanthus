9b43b410a0823f684475d362ba9d1f1ee954c73d
iosmanthus <myosmanthustree@gmail.com>
2018-02-06 10:50:30 +0800

    Functions declaration

---
+++ include/kconsole.h
@@ -0,0 +1,84 @@
+#ifndef _KCONSOLE_H_
+#define _KCONSOLE_H_
+
+#include <ktypes.h>
+
+typedef enum _VgaTextAtrr {
+  VGA_BLACK = 0,
+  VGA_BLUE,
+  VGA_GREEN,
+  VGA_CYAN,
+  VGA_RED,
+  VGA_MAGENTA,
+  VGA_BROWN,
+  VGA_LIGHT_GREY,
+  VGA_DARK_GREY,
+  VGA_LIGHT_BLUE,
+  VGA_LIGHT_GREEN,
+  VGA_LIGHT_CYAN,
+  VGA_LIGHT_RED,
+  VGA_LIGHT_MAGENTA,
+  VGA_LIGHT_BROWN, // Yellow
+  VGA_WHITE
+} VgaTextAtrr;
+
+/*
+ * Print a character on the console and move the cursor
+ * with specified attribute
+ *
+ * 'ch': character to be printed
+ * 'bg': background color
+ * 'fg': foreground color
+ *
+ * return the character's ASCII value if successed
+ * or return EOF defined in <ktypes.h>
+ * */
+i32 kconsole_putc_color( u8 ch, VgaTextAtrr bg, VgaTextAtrr fg );
+
+
+/*
+ * Print a character on the console and move the cursor
+ *
+ * 'ch': character to be printed
+ *
+ * return the character's ASCII value if successed
+ * or return EOF defined in <ktypes.h>
+ * */
+i32 kconsole_putc( u8 ch );
+
+/*
+ * Print a string on the console and move the cursor
+ * with specified attribute
+ *
+ * 'str': the string pointer to be printed
+ * 'bg': background color
+ * 'fg': foreground color
+ *
+ * return the count of character that has been printed if successed
+ * or return EOF defined in <ktypes.h>
+ * */
+i32 kconsole_puts_color( const u8 *str, VgaTextAtrr bg, VgaTextAtrr fg );
+
+
+/*
+ * Print a string on the console and move the cursor
+ *
+ * 'str': the string pointer to be printed
+ *
+ * return the count of character that has been printed if successed
+ * or return EOF defined in <ktypes.h>
+ * */
+i32 kconsole_puts( const u8 *str );
+
+/*
+ * Scroll the screen if there is no more space
+ * in the console to be written.
+ * */
+void kconsole_scroll();
+
+/*
+ * Clear the console
+ * */
+void kconsole_clear();
+
+#endif /* ifndef _KCONSOLE_H_ */
---
+++ include/ktypes.h
@@ -0,0 +1,21 @@
+#ifndef _KTYPES_H_
+#define _KTYPES_H_
+
+
+#ifndef NULL
+#define NULL (void *)0
+#endif
+
+#ifndef EOF
+#define EOF -1
+#endif /* ifndef EOF */
+
+/* Some type alias in 32-bit machine */
+typedef unsigned int u32;
+typedef int i32;
+typedef unsigned short u16;
+typedef short i16;
+typedef unsigned char u8;
+typedef char i8;
+
+#endif /* ifndef _KTYPES_H_ */
