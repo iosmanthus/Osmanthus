a3e94d377ea32fc9be40facbdd86232c32c34a44
iosmanthus <myosmanthustree@gmail.com>
2018-02-06 16:48:20 +0800

    Add some tests

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -9,18 +9,11 @@ u32 write( u8, u8, u8, u32 );
 int kmain( struct multiboot *mboot_ptr )
 {
   kconsole_clear();
-  kconsole_puts_color(
-    (const u8 *)"Welcome to Osmanthus!\n"
-                "I am a fucking stupid operating system!\n"
-                "The only thing i can do is:\n"
-                "printing some useless words on the god damn small console\n",
-    VGA_BLACK, VGA_GREEN );
-  kconsole_clear();
-  kconsole_puts_color(
-    (const u8 *)"Welcome to Osmanthus!\n"
-                "I am a fucking stupid operating system!\n"
-                "The only thing i can do is:\n"
-                "printing some useless words on the god damn small console\n",
-    VGA_BLACK, VGA_GREEN );
+  for ( int i = 0; i < 25; ++i )
+    for ( int j = 0; j < 80; ++j )
+      kconsole_puts( (const u8 *)"c" );
+
+  for ( int j = 0; j < 81; ++j )
+    kconsole_puts( (const u8 *)"O" );
   return 0;
 }
