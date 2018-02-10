b00a028baa6949ca49e5475587add5e32a2ebba0
iosmanthus <myosmanthustree@gmail.com>
2018-02-10 19:22:28 +0800

    Rename some functions: kport_in kport_out

--- driver/kport.c
+++ driver/kport.c
@@ -1,29 +1,29 @@
 #include <kport.h>
 
 
-void kport_out( u16 port, u32 data, u8 size )
+void kout( u16 port, u32 data, KDataSize size )
 {
-  if ( size == BYTE )
-    __asm__ volatile( "outb %1,%0" ::"d"( port ), "a"( data ) );
-  else if ( size == WORD )
-    __asm__ volatile( "outw %1,%0" ::"d"( port ), "a"( data ) );
-  else if ( size == DWORD )
-    __asm__ volatile( "outl %1,%0" ::"d"( port ), "a"( data ) );
+  if ( size == KBYTE )
+    __asm__ volatile( "outb %%al,%%dx" ::"d"( port ), "a"( data ) );
+  else if ( size == KWORD )
+    __asm__ volatile( "outw %%ax,%%dx" ::"d"( port ), "a"( data ) );
+  else if ( size == KDWORD )
+    __asm__ volatile( "outl %%eax,%%dx" ::"d"( port ), "a"( data ) );
 }
 
-u32 kport_in( u16 port, u8 size )
+u32 kin( u16 port, KDataSize size )
 {
   u8 db = 0;
   u16 dw = 0;
   u32 dl = 0;
-  if ( size == BYTE ) {
-    __asm__ volatile( "inb %1, %0" : "=a"( db ) : "d"( port ) );
+  if ( size == KBYTE ) {
+    __asm__ volatile( "inb %%dx, %%al" : "=a"( db ) : "d"( port ) );
     return db;
-  } else if ( size == WORD ) {
-    __asm__ volatile( "inw %1, %0" : "=a"( dw ) : "d"( port ) );
+  } else if ( size == KWORD ) {
+    __asm__ volatile( "inw %%dx, %%ax" : "=a"( dw ) : "d"( port ) );
     return dw;
-  } else if ( size == DWORD ) {
-    __asm__ volatile( "inl %1, %0" : "=a"( dl ) : "d"( port ) );
+  } else if ( size == KDWORD ) {
+    __asm__ volatile( "inl %%dx, %%eax" : "=a"( dl ) : "d"( port ) );
     return dl;
   }
   return 0;
