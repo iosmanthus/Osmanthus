09d7c788b0502f760f69873e3a002da0a0d3e3e5
iosmanthus <myosmanthustree@gmail.com>
2018-02-10 19:23:40 +0800

    Add some types

--- include/ktypes.h
+++ include/ktypes.h
@@ -18,4 +18,10 @@ typedef short i16;
 typedef unsigned char u8;
 typedef char i8;
 
+typedef enum _KDataSize {
+  KBYTE = sizeof( u8 ),
+  KWORD = sizeof( u16 ),
+  KDWORD = sizeof( u32 )
+} KDataSize;
+
 #endif /* ifndef _KTYPES_H_ */
