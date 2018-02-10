9ef7f8e2b9785d146a955b2fb1e0eba6baa6b90a
iosmanthus <myosmanthustree@gmail.com>
2018-02-10 19:24:07 +0800

    Add some tests

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,13 +1,34 @@
-#include <kconsole.h>
+#include <karg.h>
+#include <kio.h>
+#include <kport.h>
+#include <kstring.h>
 #include <ktypes.h>
+#include <kvga.h>
 
 struct multiboot {
   ;
 };
-u32 write( u8, u8, u8, u32 );
 
-int kmain( struct multiboot *mboot_ptr )
+// QEMU shudown
+void kshutdown()
 {
-  kputs( (const u8 *)"hello world\n" );
+  kout( 0xf4, 0x00, KBYTE );
+}
+
+int sum_up( int n, ... )
+{
+  kva_list arg_list;
+  kva_start( arg_list, n );
+  int sum = 0;
+  while ( n-- )
+    sum += kva_arg( arg_list, int );
+  kva_end( arg_list );
+  return sum;
+}
+
+i32 kmain( struct multiboot *mboot_ptr )
+{
+  i32 s = sum_up( 4, 1, 2, 3, 4 );
+  kprintf( "%.4s\n%20d", "hello world", 123456789 );
   return 0;
 }
