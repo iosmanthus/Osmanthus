892c2d919f3a05d6511dba188e80f975f8004ffc
iosmanthus <myosmanthustree@gmail.com>
2018-02-11 22:42:03 +0800

    Implementation of kelf_search_symbol and kget_elf_info

---
+++ kernel/kelf.c
@@ -0,0 +1,45 @@
+#include <kelf.h>
+#include <kstring.h>
+
+#define ELF32_ST_TYPE( i ) ( (i)&0xf )
+
+// Get kernel elf info
+KELF kget_kernel_elf_info( KMultiBoot *mb )
+{
+  KELFSectionHeader *begin = (KELFSectionHeader *)mb->addr;
+  KELF elf;
+
+  u32 shstrtab = begin[mb->shndx].addr; // Section name array
+  // Loop through all sections
+
+  for ( const KELFSectionHeader *sh = begin, *const end = &begin[mb->num];
+        sh < end; ++sh ) {
+    const char *section_name = (const char *)( shstrtab + sh->name );
+
+    if ( kstrcmp( section_name, ".strtab" ) == 0 ) {
+      elf.strtab = (const char *)sh->addr;
+      elf.strtabsz = sh->size;
+      continue;
+    } else if ( kstrcmp( section_name, ".symtab" ) == 0 ) {
+      elf.symtab = (KELFSymbol *)sh->addr;
+      elf.symtabsz = sh->size;
+    }
+  }
+  return elf;
+}
+
+// Return symbol whose address range includes 'addr'
+const char *kelf_search_symbol( u32 addr, const KELF *elf )
+{
+  if ( elf ) {
+    const KELFSymbol *const end =
+      &elf->symtab[elf->symtabsz / sizeof( KELFSymbol )];
+
+    for ( const KELFSymbol *symbol = elf->symtab; symbol < end; ++symbol )
+      if ( ELF32_ST_TYPE( symbol->info ) != 0x2 )
+        continue;
+      else if ( addr >= symbol->value && addr < symbol->value + symbol->size )
+        return (const char *)( elf->strtab + symbol->name );
+  }
+  return NULL;
+}
