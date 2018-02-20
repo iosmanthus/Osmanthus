617a4a2014c8b94ac54bfe56127a04446787900e
iosmanthus <myosmanthustree@gmail.com>
2018-02-20 20:30:13 +0800

    ISR header

---
+++ include/kisr.h
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2018 Liming,Deng <myosmanthustree@gmail.com>
+ * Author: Liming Deng
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+#ifndef _KINT_H_
+#define _KINT_H_
+
+#include <ktypes.h>
+
+#define IDT_ENTRY_CNT 256
+
+typedef enum _IRQ {
+  IRQ0 = 32,
+  IRQ1,
+  IRQ2,
+  IRQ3,
+  IRQ4,
+  IRQ5,
+  IRQ6,
+  IRQ7,
+  IRQ8,
+  IRQ9,
+  IRQ10,
+  IRQ11,
+  IRQ12,
+  IRQ13,
+  IRQ14,
+  IRQ15
+} IRQ;
+
+
+typedef struct _KPTRegs {
+  u32 ds; // Save previous data segment
+
+  u32 edi; // From edi to eax, all pushed by instruction: pusha
+  u32 esi;
+  u32 ebp;
+  u32 esp;
+  u32 ebx;
+  u32 edx;
+  u32 ecx;
+  u32 eax;
+
+  u32 int_id;     // Interrupt id
+  u32 error_code; // Push by CPU
+
+  /* ---- pushed by CPU ---- */
+  u32 eip;
+  u32 cs;
+  u32 eflags;
+  u32 useresp;
+  u32 ss;
+} KPTRegs;
+
+typedef void ( *KInterruptHandler )( KPTRegs * );
+
+// Register an interrupt handler function
+void kreg_int_handler( u32 int_id, KInterruptHandler handler );
+
+void kisr_handler( KPTRegs *pt_regs );
+void kirq_handler( KPTRegs *pt_regs );
+
+void isr0();
+void isr1();
+void isr2();
+void isr3();
+void isr4();
+void isr5();
+void isr6();
+void isr7();
+void isr8();
+void isr9();
+void isr10();
+void isr11();
+void isr12();
+void isr13();
+void isr14();
+void isr15();
+void isr16();
+void isr17();
+void isr18();
+void isr19();
+void isr20();
+void isr21();
+void isr22();
+void isr23();
+void isr24();
+void isr25();
+void isr26();
+void isr27();
+void isr28();
+void isr29();
+void isr30();
+void isr31();
+
+void irq0();
+void irq1();
+void irq2();
+void irq3();
+void irq4();
+void irq5();
+void irq6();
+void irq7();
+void irq8();
+void irq9();
+void irq10();
+void irq11();
+void irq12();
+void irq13();
+void irq14();
+void irq15();
+
+
+// System call
+void isr255();
+
+#endif /* ifndef _KINT_H_ */
