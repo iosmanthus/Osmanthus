b8a59bde9c3823ab216e02a02d7ce94e9ac5e2aa
iosmanthus <myosmanthustree@gmail.com>
2018-02-20 20:31:37 +0800

    GDT initialization

--- init/kgdt.c
+++ init/kgdt.c
@@ -1,23 +1,31 @@
+/*
+ * Copyright (c) 2018 Liming,Deng <myosmanthustree@gmail.com>
+ * Author: Liming Deng
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <kgdt.h>
 #include <kio.h>
 
-#define GDT_ENTRY_CNT 5
-#define GDT_ENTRY_SIZE 8
 
-#define NULL_SEG_INDEX 0
-#define CODE_SEG_INDEX 1
-#define DATA_SEG_INDEX 2
-#define USER_CODE_SEG_INDEX 3
-#define USER_DATA_SEG_INDEX 4
-
-#define GDT_CODE_SELECTOR ( GDT_ENTRY_SIZE * CODE_SEG_INDEX )
-#define GDT_DATA_SELECTOR ( GDT_ENTRY_SIZE * DATA_SEG_INDEX )
-#define GDT_USER_CODE_SELECTOR ( GDT_ENTRY_SIZE * USER_CODE_SEG_INDEX )
-#define GDT_USER_DATA_SELECTOR ( GDT_ENTRY_SIZE * USER_DATA_SEG_INDEX )
-
-typedef KSegmentDescritor KGdt[GDT_ENTRY_CNT];
-
-static KGdt gdt;
+static KSegmentDescritor gdt[GDT_ENTRY_CNT];
 
 static void set_segment_descriptor( KSegmentDescritor *sd, u32 base, u32 limit,
                                     u8 access, u8 granularity );
@@ -36,10 +44,8 @@ void set_segment_descriptor( KSegmentDescritor *sd, u32 base, u32 limit,
 KGDTPtr kinit_gdt()
 {
   KGDTPtr gdt_ptr;
-  gdt_ptr.addr = (u32)&gdt;
-  gdt_ptr.limit = sizeof( gdt );
-
-  kprintf( "%d %d\n", sizeof( gdt ), gdt_ptr.limit );
+  gdt_ptr.addr = (u32)gdt;
+  gdt_ptr.limit = sizeof( gdt ) - 1;
 
   // Null descritor
   set_segment_descriptor( &gdt[NULL_SEG_INDEX], 0x00000000, 0x00000000, 0x00,
