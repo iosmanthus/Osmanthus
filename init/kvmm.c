95c1679ede2ce90ae24f8d68d1dfd35bcb963fb4
OsmanthusTree <MyOsmanthusTree@gmail.com>
2018-03-18 12:30:25 +0800

    Enable VM and install page fault handler

--- init/kvmm.c
+++ init/kvmm.c
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2018 Liming,Deng <myosmanthustree@gmail.com>
+ * Author: Liming Deng
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <kio.h>
 #include <kmain.h>
 #include <kmultiboot.h>
@@ -131,6 +154,7 @@ void kpage_fault( KPTRegs *pt_regs )
   u32 cr2 = 0;
   // Get reg cr2 value;
   __asm__ volatile( "mov %%cr2,%0" : "=r"( cr2 ) );
+
   kprintf( "Page fault at %p, because access virtual page: %p\n", pt_regs->eip,
            cr2 );
   u32 error_code = pt_regs->error_code;
