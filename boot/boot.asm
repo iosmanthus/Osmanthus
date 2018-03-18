5ee6bb9de6486de440c1c5359768a510455a9235
iosmanthus <MyOsmanthusTree@gmail.com>
2018-03-18 13:53:17 +0800

    Simple boot entry that boot our kernel with grub

---
+++ boot/boot.asm
@@ -0,0 +1,63 @@
+; Copyright (c) 2018 Liming,Deng <myosmanthustree@gmail.com>
+; Author: Liming Deng
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy of
+; this software and associated documentation files (the "Software"), to deal in
+; the Software without restriction, including without limitation the rights to
+; use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+; the Software, and to permit persons to whom the Software is furnished to do so,
+; subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+; FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+; COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+; IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+[bits 32]
+MULTIBOOT_HEADER_MAGIC  equ     0x1badb002
+MULTIBOOT_PAGE_ALIGN    equ     1 << 0
+MULTIBOOT_MEMORY_INFO   equ     1 << 1
+MULTIBOOT_HEADER_FLAGS  equ     MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
+MULTIBOOT_CHECKSUM      equ     -(MULTIBOOT_HEADER_MAGIC+MULTIBOOT_HEADER_FLAGS)
+KERNEL_STACK_SIZE       equ     0x400
+KERNEL_VM_OFFSET        equ     0xC0000000
+
+
+section .init.text
+dd MULTIBOOT_HEADER_MAGIC
+dd MULTIBOOT_HEADER_FLAGS
+dd MULTIBOOT_CHECKSUM
+
+[global __start:function]
+[extern __kernel_multiboot_info]
+[extern kenable_paging]
+
+__start:
+    cli
+
+    mov eax, __kernel_multiboot_info
+    sub eax, KERNEL_VM_OFFSET
+    mov [eax], ebx
+
+    mov esp, __kernel_stack_top
+    and esp, 0x0fffffff0
+    mov ebp , 0x0
+    call kenable_paging
+
+    jmp __stop
+.end
+size __start __start.end - __start
+__stop:
+  hlt
+  jmp __stop
+
+section .init.data
+; Temporary kernel stack
+__kernel_stack_base:
+  times KERNEL_STACK_SIZE db 0 ; 1KB
+__kernel_stack_top:
