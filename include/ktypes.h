8d7042f1b0213974a6564bc5bf34fa7c0da47544
iosmanthus <MyOsmanthusTree@gmail.com>
2018-03-18 13:59:19 +0800

    Kernel basic types

---
+++ include/ktypes.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2018 Liming,Deng <myosmanthustree@gmail.com>
+ * Author: Liming Deng
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _KTYPES_H_
+#define _KTYPES_H_
+
+
+#ifndef NULL
+#define NULL (void *)0
+#endif
+
+#ifndef EOF
+#define EOF -1
+#endif /* ifndef EOF */
+
+/* Some type alias in 32-bit machine */
+typedef unsigned int u32;
+typedef int i32;
+typedef unsigned short u16;
+typedef short i16;
+typedef unsigned char u8;
+typedef char i8;
+
+typedef enum _KDataSize {
+  KBYTE = sizeof( u8 ),
+  KWORD = sizeof( u16 ),
+  KDWORD = sizeof( u32 )
+} KDataSize;
+
+typedef enum _KMemUnit {
+  B = 1,
+  KB = 1 << 10,
+  MB = 1 << 20,
+  GB = 1 << 30
+} KMemUnit;
+
+#endif /* ifndef _KTYPES_H_ */
